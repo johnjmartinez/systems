09eeb6fcf1ee1d23608b4f86083307b465822dda
John Martinez <johnjmartinez@gmail.com>
2017-08-25 14:59:34 -0500

    1st take at makefile not working

---
+++ shell/looper.c
@@ -0,0 +1,14 @@
+#include <yash.h>
+
+
+int main(int argc, char *argv[]) {
+  
+    while(1) { 
+        
+      /* SOMETHING, SOMETHING */  
+    
+    }
+    exit(0);
+}
+
+
---
+++ shell/yash.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
