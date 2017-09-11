2ee23dedd71ba28bfd27f912c46336c4b30b872c
John Martinez <johnjmartinez@gmail.com>
2017-09-10 19:43:26 -0500

    some top lvl signal stuff done

--- str_sign.c
+++ str_sign.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-//Safe asprintf macro
-#define SAFE_ASPRINTF(write_to, ...) { \
-    char *tmp_string_for_extend = write_to; \
-    asprintf(&(write_to), __VA_ARGS__); \
-    free(tmp_string_for_extend); \
-}
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define LINE_MAX 200
 #define	DELIM " \t"
@@ -24,20 +20,31 @@ void get_tokens(char * tokens[], char * line) {
     tokens[count] = NULL;
 }   
 
+static void sgnl_catcher(int sig_num) {
+    printf("\n- Caught signal %d\n# ",sig_num);
+    fflush(stdout);
+}
+
 
 int main(int argc, char *argv[]) {
 
     char line[LINE_MAX];
     char * _tokens[LINE_MAX/3];
+    
+    signal(SIGINT,  sgnl_catcher);   // catch ctrl+c
+    signal(SIGTSTP, sgnl_catcher);  // catch ctrl+z
 
     while(1) {
         printf("# ");
         fflush(stdout);
-    
-        if ( fgets(line, LINE_MAX, stdin) != NULL ) { // check line not null
+        if ( fgets(line, LINE_MAX, stdin) == NULL ) { // catch ctrl+d (EOF)
+            printf("\n");
+            return(0);
+        }    
+        else { // line not null
             if ( line[strlen(line)-1] != '\n' ) {   // check if line too long
                 printf("ERROR: command too long -- 200 chars max.\n");
-                while (getchar() != '\n');          // flush stdin for next call
+                while (getchar() != '\n' && getchar() != '\0'); // flush stdin 
             }
             else {
                 line[strlen(line)-1] = '\0';
@@ -47,5 +54,6 @@ int main(int argc, char *argv[]) {
         
         for( int i = 0; _tokens[i] != NULL; i++ )  printf ("%i %s\n", i, _tokens[i]);
     }
+    return(1);
 }
 
