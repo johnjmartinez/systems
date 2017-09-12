0b8059f240ce57ffb68b5fd6627b2fdbd23dd185
John M <johnjmartinez@gmail.com>
2017-09-12 15:03:17 -0500

    got idea for builtins

---
+++ str2.c
@@ -0,0 +1,16 @@
+/* strncmp example */
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char str[][5] = { "<" , ">" , "fg" };
+  char * a = "kljnosdcoinonwdc > sdiubweibiuewrbvui";
+  
+  int n;
+  printf ("Looking in \'%s\' ...\n", a);
+  for (n=0 ; n<3 ; n++)
+    printf( "%s : %d\n", str[n], strncmp(a, str[n], strlen(a)) );
+
+  return 0;
+}
