99dfc16b16ab07f6d98f500eee89a38f16191f80
John Martinez <johnjmartinez@gmail.com>
2017-10-03 23:49:18 -0500

    organizing

--- misc/TCPClient-ex1.c
+++ misc/TCPClient-ex1.c
@@ -5,33 +5,33 @@
  * Run as:
  *    TCPClient-ex1 <hostname> <port>
  */
-#include <stdio.h>
-/* socket(), bind(), recv, send */
+#include <stdio.h>      /* socket(), bind(), recv, send */
 #include <sys/types.h>
 #include <sys/socket.h> /* sockaddr_in */
 #include <netinet/in.h> /* inet_addr() */
 #include <arpa/inet.h>
-#include <netdb.h> /* struct hostent */
-#include <string.h> /* memset() */
-#include <unistd.h> /* close() */
-#include <stdlib.h> /* exit() */
+#include <netdb.h>      /* struct hostent */
+#include <string.h>     /* memset() */
+#include <unistd.h>     /* close() */
+#include <stdlib.h>     /* exit() */
 
 int main(int argc, char **argv ) {
     int	sd;
-    struct  sockaddr_in server;
+    struct sockaddr_in server;
     struct hostent *hp;
     
-    sd = socket (AF_INET,SOCK_STREAM,0);
-    
-    server.sin_family = AF_INET;
+    sd = socket (AF_INET, SOCK_STREAM, 0);
     hp = gethostbyname(argv[1]);
-    bcopy ( hp->h_addr, &(server.sin_addr.s_addr), hp->h_length);
     server.sin_port = htons(atoi(argv[2]));
-    connect(sd, (struct  sockaddr *) &server, sizeof(server));
+    server.sin_family = AF_INET;
+    
+    bcopy ( hp->h_addr, &(server.sin_addr.s_addr), hp->h_length );
+    connect ( sd, (struct  sockaddr *) &server, sizeof(server) );
+
     for (;;) {
-	send(sd, "HI", 2, 0 );
-	printf("sent HI\n");
-	sleep(2);
+        send (sd, "HI", 2, 0 );
+        printf ("sent HI\n");
+        sleep (2);
     }
 }
 
