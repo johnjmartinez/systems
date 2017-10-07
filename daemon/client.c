6ea14df2c103914db927a4b30e224a8681276185
John Martinez <johnjmartinez@gmail.com>
2017-10-06 22:03:39 -0500

    client mostly working ... switching to server-daemon

--- daemon/client.c
+++ daemon/client.c
@@ -1,6 +1,5 @@
 #include "client.h"
-/*
-CLIENT = yash <SERVER_IP_ADDR> (port is implicit = 3826)
+/*CLIENT = yash <SERVER_IP_ADDR> (port is implicit = 3826)
     protocol:
         CMD <cmd_string> \n
         CTL <[c|z|d]> \n
@@ -9,51 +8,43 @@ CLIENT = yash <SERVER_IP_ADDR> (port is implicit = 3826)
     signal handling:
         ctrl+c will send "CTL c\n" (stop cmd on server)
         ctrl+x will send "CTL z\n" (suspend cmd on server)
- */
+*/
 
 static void catch_C(int signo) { // ctrl+c
-
     // XXX -- send CTL c \n to server
-    send_cmd("CTL c \n");
-    fflush(stdout);
+    send (sckt, "CTL c \n", 7, 0);
 }
 
 static void catch_Z(int signo) { // ctrl+z
-
     // XXX -- send CTL z \n to server
-    send_cmd("CTL z \n");
-    fflush(stdout);
+    send (sckt, "CTL z \n", 7, 0);
 }
 
-int main(int argc, char* argv[]) {
+int main (int argc, char* argv[]) {
     
-    // TODO -- get arg1:SERVER_IP 
+    if (signal(SIGINT, catch_C) == SIG_ERR) printf("signal(SIGINT) error");
+    if (signal(SIGTSTP, catch_Z) == SIG_ERR) printf("signal(SIGTSTP) error");
     
     char line[LINE_MAX];
     char * _tokens[LINE_MAX / 3];
     char * tmp;
 
-    int count, sckt;
-    bool skip;
-    
+    int skip;
     struct sockaddr_in server;
-    struct hostent * host;
-    
-    if (signal(SIGINT, catch_C) == SIG_ERR) printf("signal(SIGINT) error");
-    if (signal(SIGTSTP, catch_Z) == SIG_ERR) printf("signal(SIGTSTP) error");
-    
-    // XXX -- check if connection to server code ok
+    struct hostent * target;
+       
+    // XXX -- check if connection to server code OK
     sckt = socket (AF_INET, SOCK_STREAM, 0);
-    host = gethostbyname(argv[1]);
+    target = gethostbyname(argv[1]);
     server.sin_port = htons (PORT_NUM);
     server.sin_family = AF_INET;
-    bcopy ( host->h_addr, &(server.sin_addr.s_addr), host->h_length );
+    bcopy ( target->h_addr, &(server.sin_addr.s_addr), target->h_length );
     connect ( sckt, (struct  sockaddr *) &server, sizeof(server) );
 
     while (1) {
 
         // TODO -- get command prompt from server
-        
+        printf("# ");
         fflush(stdout);
         skip = false;
 
@@ -62,18 +53,16 @@ int main(int argc, char* argv[]) {
             return (0);  // TODO -- close connection?
         }
 
-        tmp = strdup(line);
-        count = 0;
-
-        skip = tokenizer(tmp, _tokens, &count);
+        tmp = strdup (line);
+        skip = tokenizer (tmp, _tokens);
         if (skip || (_tokens[0] == NULL)) {
             free(tmp);
             continue;
         }
 
-        skip = parser(_tokens);
+        skip = parser (_tokens);
         if (skip) {
-            if (skip > 1) {
+            if (skip > 1) { // QUIT
                 printf("\n");
                 return (0); // TODO -- close connection?
             }
@@ -81,34 +70,32 @@ int main(int argc, char* argv[]) {
             continue;
         }
 
-        // TODO -- send cmd to server once it's valid : send_cmd(line)
+        // XXX -- send cmd to server
+        send (sckt, line, strlen(line), 0 );
         free(tmp);
     }
 
     return (EXIT_SUCCESS);
 }
 
-bool tokenizer(char * line, char * _tokens[], int * count) {
+bool tokenizer (char * line, char * _tokens[]) {
 
-    if (line[strlen(line) - 1] != '\n') { // check if line too long
+    if (line[strlen(line) - 1] != '\n') {   // check if line too long
         printf("ERROR: command too long -- 200 chars max\n");
-        while (getchar() != '\n'); // flush stdin
+        while (getchar() != '\n');          // flush stdin
         return true;
     }
 
     int i = 0;
-    char * pointer = strtok(line, DELIM); // strtok destroys string
+    char * pointer = strtok(line, DELIM);   // strtok destroys string
     do {
         _tokens[i++] = pointer;
     } while ((pointer = strtok(NULL, DELIM)) != NULL);
-
-    _tokens[i] = NULL; // make last token/cmd_arg NULL
-    * count = i;
-
+    
     return false;
 }
 
-int parser(char * _tokens[]) {
+int parser (char * _tokens[]) {
     
     // XXX -- check first token is either CMD, CTL or quit
     if (strncmp(_tokens[0], "CMD", 3) == 0) {
@@ -120,5 +107,6 @@ int parser(char * _tokens[]) {
     else if (strncmp(_tokens[0], "quit", 4) == 0) {
         return 2; // TODO -- check only token: _tokens[1] == NULL ? 
     }
-    return 1; // TODO -- print ERROR msg?
+    printf(" -ERROR: INVALID COMMAND\n");
+    return 1; 
 }
